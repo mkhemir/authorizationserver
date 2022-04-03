a55f10260b7aa0f86c7e0831e2e775729b32443e
mkhemir <marwen.kemir@gmail.com>
2022-04-03 18:36:46 +0200

    first commit

---
+++ src/main/java/com/certigna/authorizationserver/repository/UserDetailRepository.java
@@ -0,0 +1,13 @@
+package com.certigna.authorizationserver.repository;
+
+import com.certigna.authorizationserver.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserDetailRepository extends JpaRepository<User,Integer> {
+
+
+    Optional<User> findByUsername(String name);
+
+}
